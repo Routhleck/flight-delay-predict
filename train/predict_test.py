ea1f297e12da1e8ad698765ad66b7edddbed5583
Routhleck <1310722434@qq.com>
2022-07-09 20:17:59 +0800

    数据集重新准备完成，建模准备完成，开始测试

--- train/predict_test.py
+++ train/predict_test.py
@@ -1,25 +1,55 @@
 import joblib
 import pandas as pd
 from sklearn import preprocessing, metrics, svm
+import matplotlib.pyplot as plt
 
-# 测试数据
-data_test = [2.3,6.7,-2.1,0.0,1024.3,231.0,2.0]
-#转换为二维数组
-# 模型预测
-def predict(data):
-    # 加载模型
-    model_departure = joblib.load('train/delayDeparture.pkl')
-    model_arive = joblib.load('train/delayArive.pkl')
-    # 标准化
+# 预测
+def predict(data_train):
+
+    # 删除缺失值NaN
+    data_train.dropna(inplace=True)
+    # 为出发机场,到达机场,航班编号,年,月,日,时间 字符串转化为可训练的变量
+    data_train['出发机场'] = data_train['出发机场'].astype('category')
+    data_train['到达机场'] = data_train['到达机场'].astype('category')
+    data_train['航班编号'] = data_train['航班编号'].astype('category')
+    data_train['时间'] = data_train['时间'].astype('category')
+
+    # 字典映射编码
+    data_train['出发机场'] = data_train['出发机场'].cat.codes
+    data_train['到达机场'] = data_train['到达机场'].cat.codes
+    data_train['航班编号'] = data_train['航班编号'].cat.codes
+    data_train['时间'] = data_train['时间'].cat.codes
+
+    cols = ['出发机场','到达机场','航班编号','距离','平均温度','最高温度','最低温度','降水量','气压','风向','风速','年','月','日','时间']
+    x = data_train[cols].values
+
+    # 标准化处理
     ss_X = preprocessing.StandardScaler()
-    data = pd.DataFrame(data).T
-    data_scaled = ss_X.fit_transform(data)
-    data_pred_departure = model_departure.predict(data_scaled)
-    data_pred_arrive = model_arive.predict(data_scaled)
-    # 反标准化
-    # 反归一化
-    data_pred_departure = data_pred_departure * ss_X.scale_[0] + ss_X.mean_[0]
-    data_pred_arrive = data_pred_arrive * ss_X.scale_[0] + ss_X.mean_[0]
-    return data_pred_departure, data_pred_arrive
+    X_train_scaled = ss_X.fit_transform(x)
+    # 读取模型
+    model = joblib.load('train/delayPredict.pkl')
+    # 预测
+    delay_cond = model.predict(X_train_scaled)
+    # 返回预测结果
+    return delay_cond
 
-print(predict(data_test))
+# 分类模型评估
+def evaluate(y_true, y_pred):
+    '''print('Accuracy:', metrics.accuracy_score(y_true, y_pred))
+    print('Recall:', metrics.recall_score(y_true, y_pred, average='weighted'))
+    print('Precision:', metrics.precision_score(y_true, y_pred, average='weighted'))
+    print('F1:', metrics.f1_score(y_true, y_pred, average='weighted'))'''
+
+    ACC = metrics.accuracy_score(y_true, y_pred)
+    RECALL = metrics.recall_score(y_true, y_pred, average='weighted')
+    PREC = metrics.precision_score(y_true, y_pred, average='weighted')
+    F1 = metrics.f1_score(y_true, y_pred, average='weighted')
+    return ACC, RECALL, PREC, F1
+    # 混淆矩阵热力图
+    # plt.matshow(metrics.confusion_matrix(y_true, y_pred))
+    # plt.show()
+
+# 模型评估
+def evaluate_model(y_validation, y_validation_pred):
+    ACC, RECALL, PREC, F1 = evaluate(y_validation, y_validation_pred)
+    return ACC, RECALL, PREC, F1
--- train/train.py
+++ train/train.py
@@ -9,85 +9,109 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.multioutput import MultiOutputRegressor
 import joblib
 import xgboost as xgb
+from predict_test import evaluate_model
 
-# 导入训练集
+def train_test(max_depth, learning_rate, objective, booster, n_jobs, min_child_weight, gamma, subsample, colsample_bytree, scale_pos_weight):
+    # 导入训练集
 
-data_train = pd.read_csv('dataset/flight_dataset.csv')
+    data_train = pd.read_csv('../dataset/flight_final.csv', encoding= 'utf-8')
 
-# 删除缺失值NaN
-data_train.dropna(inplace=True)
+    # 删除缺失值NaN
+    data_train.dropna(inplace=True)
+    # 为出发机场,到达机场,航班编号,年,月,日,时间 字符串转化为可训练的变量
+    data_train['出发机场'] = data_train['出发机场'].astype('category')
+    data_train['到达机场'] = data_train['到达机场'].astype('category')
+    data_train['航班编号'] = data_train['航班编号'].astype('category')
+    data_train['时间'] = data_train['时间'].astype('category')
 
-cols = ['平均温度', '最高温度', '最低温度', '降水量', '气压', '风向', '风速']
-x = data_train[cols].values
-y = data_train[['出发延迟']].values
+    # 字典映射编码
+    data_train['出发机场'] = data_train['出发机场'].cat.codes
+    data_train['到达机场'] = data_train['到达机场'].cat.codes
+    data_train['航班编号'] = data_train['航班编号'].cat.codes
+    data_train['时间'] = data_train['时间'].cat.codes
+
+    cols = ['出发机场','到达机场','航班编号','距离','平均温度','最高温度','最低温度','降水量','气压','风向','风速','年','月','日','时间']
+    x = data_train[cols].values
+    y = data_train[['延迟程度']].values
+
+    # 取5%作为训练验证集
 
 
-# 划分数据集
-X_train, X_validation, y_train, y_validation = train_test_split(x, y, test_size=0.2, random_state=42)
+    # 划分数据集
+    X_train, X_validation, y_train, y_validation = train_test_split(x, y, test_size=0.05, random_state=42)
 
-# 标准化处理
-ss_X = preprocessing.StandardScaler()
-ss_Y = preprocessing.StandardScaler()
-X_train_scaled = ss_X.fit_transform(X_train)
-y_train_scaled = ss_Y.fit_transform(y_train.reshape(-1, 1))
-print(X_train_scaled)
-X_validation_scaled = ss_X.transform(X_validation)
-y_validation_scaled = ss_Y.transform(y_validation.reshape(-1, 1))
+    # 标准化处理
+    ss_X = preprocessing.StandardScaler()
+    X_train_scaled = ss_X.fit_transform(X_train)
+    print(X_train_scaled)
+    X_validation_scaled = ss_X.transform(X_validation)
 
 
-# 建模(注意多标签的问题)
+    # 建模(注意多标签的问题)
 
-xgb_model = xgb.XGBRegressor(max_depth=3,
-                             learning_rate=0.1,
-                             n_estimators=100,
-                             objective='reg:squarederror',
-                             booster='gbtree',
-                             random_state=0)
+    xgb_model = xgb.XGBClassifier(max_depth=max_depth,
+                                learning_rate= learning_rate,
+                                objective= objective,
+                                booster= booster,
+                                n_jobs= n_jobs,
+                                min_child_weight= min_child_weight,
+                                gamma= gamma,
+                                subsample= subsample,
+                                colsample_bytree= colsample_bytree,
+                                scale_pos_weight= scale_pos_weight
+                                )
 
+    # 使用cv函数确定n_estimators的数量
+    xgb_param = xgb_model.get_xgb_params()
+    xgtrain = xgb.DMatrix(X_train_scaled, label=y_train)
+    cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=xgb_model.get_params()['n_estimators'], nfold=5, metrics='auc', early_stopping_rounds=50)
+    xgb_model.set_params(n_estimators=cvresult.shape[0])
+
+
+    '''
+    xgb_model = MultiOutputRegressor(xgb.XGBRegressor(max_depth=3,
+                                learning_rate=0.1,
+                                n_estimators=100,
+                                objective='reg:squarederror',
+                                booster='gbtree',
+                                random_state=0))
+    '''
+
+    # 拟合
+    xgb_model.fit(X_train_scaled, y_train)
+    # 预测
+    y_validation_pred = xgb_model.predict(X_validation_scaled)
+
+    # data_pred_departure = data_pred_departure * ss_X.scale_[0] + ss_X.mean_[0]
+
+    # 保存模型
+    joblib.dump(xgb_model, 'train/delayPredict.pkl')
+
+    # 评估模型
+    ACC, RECALL, PREC, F1 = evaluate_model(y_validation, y_validation_pred)
+    return ACC, RECALL, PREC, F1
 '''
-xgb_model = MultiOutputRegressor(xgb.XGBRegressor(max_depth=3,
-                             learning_rate=0.1,
-                             n_estimators=100,
-                             objective='reg:squarederror',
-                             booster='gbtree',
-                             random_state=0))
-'''
-
-# 拟合
-xgb_model.fit(X_train_scaled, y_train_scaled)
-# 预测
-y_validation_pred = xgb_model.predict(X_validation_scaled)
-# 反标准化
-y_validation_pred = y_validation_pred * ss_Y.scale_[0] + ss_Y.mean_[0]
-# data_pred_departure = data_pred_departure * ss_X.scale_[0] + ss_X.mean_[0]
-# 保存模型
-joblib.dump(xgb_model, 'train/delayPredict.pkl')
-
-
 plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 # 画图
-plt.plot(range(y_validation_scaled.shape[0]), y_validation_scaled, color="blue", linewidth=1.5, linestyle="-")
+plt.plot(range(y_validation.shape[0]), y_validation, color="blue", linewidth=1.5, linestyle="-")
 plt.plot(range(y_validation_pred.shape[0]), y_validation_pred, color="red", linewidth=1.5, linestyle="-.")
 plt.legend(['真实值', '预测值'])
 plt.title("出发延迟真实值与预测值比对图")
 plt.show()  #显示图片
 
 
-# 模型评估
-print('可解释方差值：{}'.format(round(metrics.explained_variance_score(y_validation_scaled, y_validation_pred), 2)))
-print('平均绝对误差：{}'.format(round(metrics.mean_absolute_error(y_validation_scaled, y_validation_pred), 2)))
-print('均方误差：{}'.format(round(metrics.mean_squared_error(y_validation_scaled, y_validation_pred), 2)))
-print('R方值：{}'.format(round(metrics.r2_score(y_validation_scaled, y_validation_pred), 2)))
+# 分类模型
 
 # 显示重要特征
 importances = list(xgb_model.feature_importances_)
-data_tmp=data_train.drop(columns=['出发延迟'])
+data_tmp=data_train.drop(columns=['延迟程度'])
 # data_tmp=data_tmp.drop(columns=['到达延迟'])
 # data_tmp=data_train.drop(columns=['到达延迟'])
 feature_list = list(data_tmp.columns)
 
 feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
 feature_importances = sorted(feature_importances, key=lambda x: x[1], reverse=True)
+'''
 
